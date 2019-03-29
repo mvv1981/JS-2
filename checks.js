1884f751aeab377e8879e4a4c3d2fbe9087540f0
Evgenia <Engelis7865@gmail.com>
2019-03-29 20:50:58 +0400

    Сделала задание

--- checks.js
+++ checks.js
@@ -6,5 +6,8 @@ var isValidTime = require('./index.js');
 
 assert.equal(isValidTime(12, 30), true, 'Время 12:30 валидное.');
 assert.equal(isValidTime(12, 61), false, 'Время 12:61 невалидное');
+assert.equal(isValidTime(-7, 0), false, 'время -7:0 невалидное');
+assert.equal(isValidTime(23, 59), true, 'время 23:59 валидное');
+
 
 console.info('OK!');
